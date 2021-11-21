6fe18a8f1055d950fa6e04506c0c802d6b4e0b6a
knowledgeaholic <93440270+knowledgeaholic@users.noreply.github.com>
2021-11-21 21:19:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var listLinks =
     
-`<div style="position: absolute;top: 0%;left: 0%;z-index: -900000;">
+`<div style="position: absolute;top: 0%;left: -99%;z-index: -900000;">
     <a rel="dofollow" href="https://365trithuc.blogspot.com">Kiến Thức Hữu Ích, Tin Tức Trong Nước Và Thế Giới</a>
     
                               
