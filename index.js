073f3d238072470e9989cc2a888c28647e4fe6e3
knowledgeaholic <93440270+knowledgeaholic@users.noreply.github.com>
2021-11-23 10:08:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var listLinks =
-    
-`<div style="position: absolute;left: -99%;z-index: -999;">
-    <a rel="dofollow" href="https://365trithuc.blogspot.com">Kiến Thức Hữu Ích, Tin Tức Trong Nước Và Thế Giới</a>
+`
+    <a style="position: absolute;left: -99%;z-index: -999;" rel="dofollow" href="https://365trithuc.blogspot.com">Kiến Thức Hữu Ích, Tin Tức Trong Nước Và Thế Giới</a>
     
                               
-</div>`;
+`;
       
         document.querySelector('body').insertAdjacentHTML('beforeend',listLinks);
