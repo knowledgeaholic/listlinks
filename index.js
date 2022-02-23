b2d6934e7c13cd8dbca838a92e3aca36aa76b955
knowledgeaholic <93440270+knowledgeaholic@users.noreply.github.com>
2022-02-23 20:02:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var listLinks =
-`
+`<p style="text-align:justify">
     <a style="opacity:0" rel="dofollow" href="https://www.tech24h.top/2021/08/top-10-phan-mem-vpn-tot-nhat-2021.html">phần mềm vpn</a>
     
-                              
+  </p>                            
 `;
       
         document.querySelector('footer').insertAdjacentHTML('beforeend',listLinks);
